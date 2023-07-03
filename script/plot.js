79d492361c955955c515a0a44a6a7a3283646003
jlindstrom12 <jmlindst@andrew.cmu.edu>
2023-07-03 14:35:58 -0400

    fixing graph

---
+++ script/plot.js
@@ -0,0 +1,33 @@
+var year1week = {
+    x: ['F21','S22','F22','S23'],
+    y: [3.18, 3.63, 3.79, 3.82],
+    mode: 'lines+text+markers',
+    // text: ['3.2', '3.6', '3.79', '3.82'],
+    textposition: 'top',
+    name:'Semester',
+    line: {
+        color: 'rgb(219, 64, 82)'
+    }
+  };
+  
+  var year2week = {
+    x: ['F21','S22','F22','S23'],
+    y: [3.18, 3.4, 3.53, 3.61],
+    mode: 'lines+text+markers',
+    // text: [3.2, 3.2, 3.2],
+    textposition: 'top',
+    name:'Cumulative'
+  };
+  
+  
+  var data = [ year1week, year2week ];
+  
+  var layout = {
+    title:'GPA',
+    xaxis:{title:{text:'Semester'}},
+    yaxis:{title:{text:'GPA'}},
+    paper_bgcolor: 'rgb(221,221,221)'
+    
+  };
+  
+  Plotly.newPlot('mydays', data, layout);
